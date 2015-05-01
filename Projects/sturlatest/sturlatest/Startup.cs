b8eca79ad782f1d1f878d36713a6cb3bbbf3f009
Sturla <sturlinn@gmail.com>
2015-05-01 10:33:44 +0000

    prufa

---
+++ Projects/sturlatest/sturlatest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sturlatest.Startup))]
+namespace sturlatest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
